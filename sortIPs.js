488b17fab5c99227d53d05f07af67ebb9a4ed4c8
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2024-01-16 13:04:26 +0530

    updates

--- sortIPs.js
+++ sortIPs.js
@@ -1,11 +1,12 @@
 const fs = require("node:fs");
+const ip = require("ip");
 
 const main = () => {
   let ips = fs.readFileSync("IPv4 List.txt").toString().split("\n");
-  let sortedIps = ips.sort((a, b) => a.localeCompare(b));
+  let sortedIps = ips.filter((IP) => ip.isV4Format(IP)).sort((a, b) => a.localeCompare(b));
   fs.writeFileSync("IPv4 List.txt", sortedIps.join("\n"));
   ips = fs.readFileSync("IPv6 List.txt").toString().split("\n");
-  sortedIps = ips.sort((a, b) => a.localeCompare(b));
+  sortedIps = ips.filter((IP) => ip.isV6Format(IP)).sort((a, b) => a.localeCompare(b));
   fs.writeFileSync("IPv6 List.txt", sortedIps.join("\n"));
 };
 
