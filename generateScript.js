53a9a3122691ff59fb087afcb77d95e4e8632972
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2024-01-20 14:31:48 +0530

    updates

--- generateScript.js
+++ generateScript.js
@@ -3,8 +3,8 @@ const fs = require("node:fs");
 const main = () => {
   const ipv4List = fs.readFileSync("IPv4 List.txt").toString().split("\n");
   const ipv6List = fs.readFileSync("IPv6 List.txt").toString().split("\n");
-  const prefix = "ufw deny from ";
-  const suffix = " to any";
+  const prefix = "ufw deny from \"";
+  const suffix = "\" to any";
   let script = "";
   for(const ip of ipv4List) {
     script += `${prefix}${ip}${suffix}\n`;
